7458c8f283e451006466c24066f1bf085da80883
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-12-19 11:29:23 +0000

    Dont know

---
+++ src/components/item/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Item = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Item.propTypes = {
+
+};
+
+export default Item;
